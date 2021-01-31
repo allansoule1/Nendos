5211221073b3565df7f0ed69807b0f3960ec2c45
Charlie.LUCAS <charlie.lucas@airliquide.com>
2021-01-31 23:03:44 +0100

    contact&legal

--- pages/legal/index.js
+++ pages/legal/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Head from 'next/head'
+import { Content } from "../../components/homepage";
 import {Footer, Layout} from '../../components'
 import {Header, Card} from '../../components/List';
 import Link from "next/link";
 
-export default function List({parts}) {
+export default function LegalNotices() {
     const urlBackground = "./background.jpg";
+    const contentText1 = [
+      "Copyright (c) janvier 2021 ChibiMarket Licence Libre MIT", "L’autorisation est accordée, gracieusement, à toute personne acquérant une copie de ce logiciel et des fichiers de documentation associés (le ‘logiciel’), de commercialiser le logiciel sans restriction, notamment les droits d’utiliser, de copier, de modifier, de fusionner, de publier, de distribuer, de sous-licencier et/ou de vendre des copies du logiciel, ainsi que d’autoriser les personnes auxquelles le logiciel est fournie à le faire, sous réserve des conditions suivantes :",
+ "La déclaration de copyright ci-dessus et la présente autorisation doivent être incluses dans toutes copies ou parties substantielles du logiciel.",
+  "Le logiciel est fourni ‘tel quel’, sans garantie d’aucune sorte, explicite ou implicite, notamment sans garantie de qualité marchande, d’adéquation à un usage particulier et d’absence de contrefaçon. En aucun cas, les auteurs ou titulaires du droit d’auteur ne seront responsables de tout dommage, réclamation ou autre responsabilité, que ce soit dans le cadre d’un contrat, d’un délit ou autre, en provenance de, consécutif à ou en relation avec le logiciel ou son utilisation, ou avec d’autres éléments du logiciel."
+    ];
+
+
     return (
         <>
             <Head>
-                <title>Nendo parts</title>
+                <title>Mentions légales</title>
             </Head>
 
             <Header urlBackground={urlBackground}/>
-            Legal
+            <Content text={contentText1}/>
             <Footer/>
 
             <style jsx global>{`
@@ -37,16 +45,4 @@ export default function List({parts}) {
         </>
 
     )
-}
-
-export async function getStaticProps(context) {
-    const res = await fetch("http://localhost:3000/nendoparts");
-    console.log(res);
-    const parts = await res.json();
-
-    return {
-        props: {
-            parts
-        }
-    }
 }
